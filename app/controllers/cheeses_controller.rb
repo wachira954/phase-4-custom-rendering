af22b9a4c7722043155468126d5291aa62b2bd0a
wachira <wachiraa954@gmail.com>
2022-09-28 11:51:29 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,48 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
-  def show
-    cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+  # def show
+    # def show
+    #   cheese = Cheese.find_by(id: params[:id])
+    #   if cheese
+    #     render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    #   else
+    #     # status: :not_found will produce a 404 status code
+    #     render json: { error: 'Cheese not found' }, status: :not_found
+    #   end
+    # end
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   if cheese
+  #     render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  #   else
+  #     render json: { error: 'Cheese not found' }
+  #   end
+  # end
+#     cheese = Cheese.find_by(id: params[:id])
+#   render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+#   end
+# end
+#     cheese = Cheese.find_by(id: params[:id])
+#     render json: {
+#       id: cheese.id,
+#       name: cheese.name,
+#       price: cheese.price,
+#       is_best_seller: cheese.is_best_seller
+#     }
+#   end
+# end
+def show
+  cheese = Cheese.find_by(id: params[:id])
+  render json: {
+    id: cheese.id,
+    name: cheese.name,
+    price: cheese.price,
+    is_best_seller: cheese.is_best_seller
+  }
+end
 
 end
